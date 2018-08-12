8d9397c7549e0852e0280a3c2ea89183f99096d0
MD SADIK HUSSAIN <sadik.nitp18@gmail.com>
2018-08-12 15:47:37 +0530

    sommit simple math code

---
+++ basic/simpleMath.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+# constant variables
+a=tf.constant(6.5, name='A')
+b=tf.constant(3.4, name='B')
+c=tf.constant(3.0, name='C')
+d=tf.constant(100.2, name='D')
+
+# a^2
+square = tf.square(a,name='square')
+
+# b ^ c
+power = tf.pow(b,c,name='pow')
+
+# d ^ -1/2
+sqrt = tf.sqrt(d,name='sqrt')
+
+# square + power + sqrt
+final_sum = tf.add_n([square,power,sqrt],name='final_sum')
+
+# start session to run tesor network
+sess = tf.Session()
+
+print "square of a : " , sess.run(square)
+print "power of b^c : ", sess.run(power)
+print "square root of d : " , sess.run(sqrt)
+print "sum of square, power, sqrt : ", sess.run(final_sum)
+
+# close session
+sess.close()
